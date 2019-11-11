67e8d1b36ce792b1aaedfa5146d250417ae96fc8
Ryan Fielder <ryan.fielder@hotmail.com>
2019-11-11 17:08:29 -0600

    working through thinkful modules

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import Split from './composition/Split';
 import './App.css';
+import TheDate from './state/TheDate';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className='App'>
+      <Split className="left" flexBasis={2}>Left Split</Split>
+      <Split className="right">Right Split</Split>
+      <TheDate />
+    </main>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,14 @@
+// make React available
 import React from 'react';
+
+//make the ReactDOM available, necessary for rendering the component
 import ReactDOM from 'react-dom';
+
+//make the App component available
 import App from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+})
